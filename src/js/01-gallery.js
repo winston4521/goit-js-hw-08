5b7314773b1668a175b30b5438aef2c9eaf652f0
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2022-08-29 20:17:13 +0200

    sd

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+const imagesMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', imagesMarkup);
+
+// Create and map elements
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item"
+        href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}">
+       </a>
+`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
