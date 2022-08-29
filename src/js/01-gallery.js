74565a36dd6732060b95d1aa97e9040f1f6de695
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2022-08-29 20:41:29 +0200

    ss

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,9 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryContainer = document.querySelector('.gallery');
 
