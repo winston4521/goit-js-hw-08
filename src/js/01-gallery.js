3245091ad6e21bfd4be14d9d2129d810ed0174d9
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2022-08-30 13:09:49 +0200

    ss

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,5 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// Описаний в документації
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+// import Player from '@vimeo/player';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+// player.on('timeupdate', function (data) {
+//   // data is an object containing properties specific to that event
+//   console.log(data);
+// });
 
