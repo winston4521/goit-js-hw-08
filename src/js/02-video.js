a05363e55889187ae9aff67ebe564874a17fb7f6
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2022-08-30 13:25:52 +0200

    ss

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,17 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 
-// player.on('timeupdate', function (data) {
-//   // data is an object containing properties specific to that event
-//   console.log(data);
-// });
-
+player.on('timeupdate', function (data) {
+  // data is an object containing properties specific to that event
+  console.log(data);
+});
